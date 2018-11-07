b409e1227cbebe55f13bdb39fed02b48f92da9cd
Francis <fdoumet@gmail.com>
2018-11-07 15:24:51 -0800

    Change referral email wording

--- templates/email.referral_plaintext.php
+++ templates/email.referral_plaintext.php
@@ -1,4 +1,4 @@
 <?php
-echo "Hi there, " . p($_['referrer'] . ' (' . $_['referrer_email'] . ')') . "thinks you should use " . $_['sitename']."! It's a free and easy way to access and share your docs, photos, and videos with colleagues and friends, even if they don't have " . $_['sitename'] . " accounts. Get started by clicking on the following link, and you'll get an extra 512 MB added to your 1 GB of free space.";
+echo "Hi there, " . p($_['referrer'] . ' (' . $_['referrer_email'] . ')') . "thinks you should use " . $_['sitename']."! It’s a free service that uses AI to compress photos down to as little as 10% of their original size, without compromising on quality or resolution. You can also store and share your photos, documents, and videos with people, even if they don't have " . $_['sitename'] . " accounts. Get started by clicking on the following link, and you'll get an extra 512 MB added to your 1 GB of free space.";
 echo "\n\n".$_['link'];
 echo "\n\nHappy " . $_['sitename'] . "ing!";
