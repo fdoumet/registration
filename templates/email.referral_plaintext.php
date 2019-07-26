f272ca269e22922b53b9c3f2202dc9dd0f3499b6
Francis <fdoumet@gmail.com>
2019-07-26 14:19:53 -0700

    Email wording

--- templates/email.referral_plaintext.php
+++ templates/email.referral_plaintext.php
@@ -1,4 +1,4 @@
 <?php
-echo "Hi there, " . p($_['referrer'] . ' (' . $_['referrer_email'] . ')') . "thinks you should use " . $_['sitename']."! It’s a free service that uses AI to compress photos down to as little as 10% of their original size, without compromising on quality or resolution. You can also store and share your photos, documents, and videos with people, even if they don't have " . $_['sitename'] . " accounts. Get started by clicking on the following link, and you'll get an extra 512 MB added to your 1 GB of free space.";
+echo "Hi there, " . p($_['referrer'] . ' (' . $_['referrer_email'] . ')') . "thinks you should use " . $_['sitename']."! It’s a free service that uses AI to compress photos down to as little as 10% of their original size, without compromising on quality or resolution. You can also store and share your photos, documents, and videos with people, even if they don't have " . $_['sitename'] . " accounts. Get started by clicking on the following link, and you'll get an extra 512 MB added to your 1 GB of free space, and an extra 5 image compressions a month for a total of 15/month.";
 echo "\n\n".$_['link'];
-echo "\n\nHappy " . $_['sitename'] . "ing!";
+echo "\n\nHappy " . $_['sitename'] . "-ing!";
